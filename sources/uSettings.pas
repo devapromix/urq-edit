3ce3d096ffe7b702e2be3e84bfef50b130005043
devapromix <bees@meta.ua>
2018-07-25 10:58:42 +0300

    Настройки.

---
+++ sources/uSettings.pas
@@ -0,0 +1,89 @@
+﻿unit uSettings;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, Buttons, ComCtrls;
+
+type
+  TfSettings = class(TForm)
+    btOK: TBitBtn;
+    btCancel: TBitBtn;
+    PageControl1: TPageControl;
+    TabSheet1: TTabSheet;
+    Label4: TLabel;
+    btSelURQ: TSpeedButton;
+    edSelURQ: TEdit;
+    OpenDialog: TOpenDialog;
+    procedure btSelURQClick(Sender: TObject);
+    procedure btOKClick(Sender: TObject);
+    procedure btCancelClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+    procedure LoadConfig;
+    procedure SaveConfig;
+  end;
+
+var
+  fSettings: TfSettings;
+
+implementation
+
+{$R *.dfm}
+
+uses uCommon, uUtils, IniFiles;
+
+procedure TfSettings.btCancelClick(Sender: TObject);
+begin
+  LoadConfig;
+end;
+
+procedure TfSettings.btOKClick(Sender: TObject);
+begin
+  SaveConfig;
+end;
+
+procedure TfSettings.btSelURQClick(Sender: TObject);
+begin
+  // Выбор интерпретатора для запуска квестов
+  OpenDialog.Filter := stURQIntFilters;
+  if OpenDialog.Execute then
+    edSelURQ.Text := Trim(OpenDialog.FileName);
+end;
+
+procedure TfSettings.FormCreate(Sender: TObject);
+begin
+  LoadConfig;
+end;
+
+procedure TfSettings.LoadConfig;
+var
+  F: TIniFile;
+begin
+  F := TIniFile.Create(Utils.GetPath('') + 'config.ini');
+  try
+    // Интерпретатор
+    edSelURQ.Text := Trim(F.ReadString('Main', 'URQInt', ''));
+  finally
+    FreeAndNil(F);
+  end;
+end;
+
+procedure TfSettings.SaveConfig;
+var
+  F: TIniFile;
+begin
+  F := TIniFile.Create(Utils.GetPath('') + 'config.ini');
+  try
+    // Интерпретатор
+    F.WriteString('Main', 'URQInt', Trim(edSelURQ.Text));
+  finally
+    FreeAndNil(F);
+  end;
+end;
+
+end.
